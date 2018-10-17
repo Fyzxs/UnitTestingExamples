631ba37d903c11eed3e09dade965e763b62bf21d
Quinn <Fyzxs@users.noreply.github.com>
2018-10-16 19:46:21 -0700

    Adding example for C# in Visual Studio with MsUnit testing framework

---
+++ visualstudio/csharp/msunit/CSharpUnitTestExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpUnitTestExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CSharpUnitTestExample")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c97121de-6990-4236-97f3-10fa4528b0d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ visualstudio/csharp/msunit/CSharpUnitTestExample/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpUnitTestExample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnString1GivenInteger1()
+        {
+            //Arrange
+            int input = 1;
+            string expected = "1";
+
+            //Act
+            string actual = FizzBuzzIt(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldReturnString2GivenInteger2()
+        {
+            //Arrange
+            int input = 2;
+            string expected = "2";
+
+            //Act
+            string actual = FizzBuzzIt(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void ShouldReturnStringFizzGivenInteger3()
+        {
+            //Arrange
+            int input = 3;
+            string expected = "Fizz";
+
+            //Act
+            string actual = FizzBuzzIt(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private string FizzBuzzIt(int toFizzBuzz)
+        {
+            return toFizzBuzz.ToString();
+        }
+    }
+}
